d28e2694a2274c6098d9822c02ad1410df9a6f8a
jagtapK <jagtapkajal54@gmail.com>
2025-08-31 09:42:50 +0530

    Added service to implement resident

--- src/main/java/carveo_portal/carveoManagement/service/ResidentService.java
+++ src/main/java/carveo_portal/carveoManagement/service/ResidentService.java
@@ -1,8 +1,12 @@
 package carveo_portal.carveoManagement.service;
 
 import carveo_portal.carveoManagement.entity.Resident;
+import carveo_portal.carveoManagement.entity.Vehicle;
 import carveo_portal.carveoManagement.repository.ResidentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ResidentService {
@@ -13,7 +17,20 @@ public class ResidentService {
         this.residentRepository = residentRepository;
     }
 
-    public Resident addResident(Resident resident){
+    public Resident saveResident(Resident resident){
         return residentRepository.save(resident);
+    }
+
+    public List<Resident> saveAll(List<Resident> residents) {
+        for (Resident resident : residents) {
+            for (Vehicle v : resident.getVehicleList()) {
+                v.setResident(resident);
+            }
+        }
+        return residentRepository.saveAll(residents);
+    }
+
+    public Optional<Resident> getResidentById(int id){
+        return residentRepository.findById(id);
     }
 }
