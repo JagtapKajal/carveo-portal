da007050bc41d25f9dda2ce8446c90f33e2492e8
jagtapK <jagtapkajal54@gmail.com>
2025-08-31 09:41:49 +0530

    Implement resource not found exception for confirmation

---
+++ src/main/java/carveo_portal/carveoManagement/exceptionHandling/ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+package carveo_portal.carveoManagement.exceptionHandling;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }}
