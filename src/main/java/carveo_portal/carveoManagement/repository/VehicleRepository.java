757c20c89386389f6f3f3c408ff8c39f01c51be3
jagtapK <jagtapkajal54@gmail.com>
2025-09-01 22:48:08 +0530

    Update vehicle repository add repository to get vehicle details

--- src/main/java/carveo_portal/carveoManagement/repository/VehicleRepository.java
+++ src/main/java/carveo_portal/carveoManagement/repository/VehicleRepository.java
@@ -3,5 +3,8 @@ package carveo_portal.carveoManagement.repository;
 import carveo_portal.carveoManagement.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
 }
