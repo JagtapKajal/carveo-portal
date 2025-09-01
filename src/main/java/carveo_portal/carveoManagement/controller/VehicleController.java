1d2cf9eb503026040244602d531736d5865613e0
jagtapK <jagtapkajal54@gmail.com>
2025-09-01 09:31:11 +0530

    Implement Vehicle controller to get vehicle details by APIs

---
+++ src/main/java/carveo_portal/carveoManagement/controller/VehicleController.java
@@ -0,0 +1,29 @@
+package carveo_portal.carveoManagement.controller;
+
+import carveo_portal.carveoManagement.entity.Resident;
+import carveo_portal.carveoManagement.entity.Vehicle;
+import carveo_portal.carveoManagement.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/vehicles")
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @PostMapping("/createVehicle")
+    public ResponseEntity<?> createVehicle(@RequestBody Vehicle vehicle) {
+        return vehicleService.createVehicle(vehicle);
+    }
+}
