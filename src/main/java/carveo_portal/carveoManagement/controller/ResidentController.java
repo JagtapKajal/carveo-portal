2c263845940a763fc2ee9d19428ac9e107da65d2
jagtapK <jagtapkajal54@gmail.com>
2025-09-01 09:29:47 +0530

    Write APIs to get resident by id, by first name and last name

--- src/main/java/carveo_portal/carveoManagement/controller/ResidentController.java
+++ src/main/java/carveo_portal/carveoManagement/controller/ResidentController.java
@@ -3,6 +3,7 @@ package carveo_portal.carveoManagement.controller;
 import carveo_portal.carveoManagement.entity.Resident;
 import carveo_portal.carveoManagement.exceptionHandling.ResourceNotFoundException;
 import carveo_portal.carveoManagement.service.ResidentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,5 +37,30 @@ public class ResidentController {
         return residentService.getResidentById(id).orElseThrow(()-> new ResourceNotFoundException("Resident not found with id: " +id));
     }
 
+    @GetMapping("/getAllResident")
+    public ResponseEntity<List<Resident>> getAllResidents() {
+        return ResponseEntity.ok(residentService.getAllResident());
+    }
+
+    @GetMapping("/getByName")
+    public ResponseEntity<?> getResidentByFnameAndLname(
+            @RequestParam String fname,
+            @RequestParam String lname) {
+
+
+        // Validation: no numeric values allowed
+        if (!fname.matches("[a-zA-Z]+") || !lname.matches("[a-zA-Z]+")) {
+            return ResponseEntity.badRequest().body("First Name and Last Name should not contain numbers.");
+        }
+
+        //it will find resident from database
+        Resident resident = residentService.getResidentByFnameAndLname(fname, lname);
+
+        //if data is not found in db
+        if (resident == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No resident found with the name " + fname + " " + lname);
+        }
+        return ResponseEntity.ok(resident);
+    }
 }
 
