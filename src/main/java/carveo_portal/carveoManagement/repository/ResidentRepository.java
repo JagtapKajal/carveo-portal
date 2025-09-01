b22c5c77b53cb3c17515ab5e078e3c72d74ca18f
jagtapK <jagtapkajal54@gmail.com>
2025-09-01 09:30:05 +0530

    Created Repo

--- src/main/java/carveo_portal/carveoManagement/repository/ResidentRepository.java
+++ src/main/java/carveo_portal/carveoManagement/repository/ResidentRepository.java
@@ -3,5 +3,7 @@ package carveo_portal.carveoManagement.repository;
 import carveo_portal.carveoManagement.entity.Resident;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ResidentRepository extends JpaRepository<Resident, Integer> {
-}
+    Resident findByFnameAndLname(String fname, String lname);}
