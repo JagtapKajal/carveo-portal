9e70e7cc5da2dce39b4b54acef6936eb1172169f
jagtapK <jagtapkajal54@gmail.com>
2025-09-03 22:19:16 +0530

    T021 - Created DTO file to handle particular visitor fields

---
+++ src/main/java/carveo_portal/carveoManagement/VisitorRequestDTO.java
@@ -0,0 +1,50 @@
+package carveo_portal.carveoManagement;
+
+import carveo_portal.carveoManagement.enums.VisitorType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class VisitorRequestDTO {
+
+    @NotBlank(message = "name should not be blank")
+    private String visitorname;
+
+    private String vehiclename;
+
+    @NotBlank(message = "Registration number should not be blank")
+    private String vehicleRegistrationNumber;
+
+    @NotNull(message = "Visitor purpose is mandatory")
+    private String visitpurpose;
+
+    @NotNull(message = "Time is required")
+    private LocalDateTime timein;
+
+    private LocalDateTime timeout;
+
+    private boolean isActiveVisitor = true;
+
+
+    @NotNull(message = "Phone number is mandatory")
+    private Long phonenumber;
+
+    @NotNull(message = "Visitor type is mandatory")
+   private VisitorType visitorType;
+
+    @NotNull(message = "Resident Id is required")
+   private int residentId;
+
+    public int getResidentId() {
+        return residentId;
+    }
+
+    public void setResidentId(int residentId) {
+        this.residentId = residentId;
+    }
+}
