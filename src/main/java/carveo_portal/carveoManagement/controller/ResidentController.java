1ac334549dfd232d183fb5a787221cf56cf91ee8
jagtapK <jagtapkajal54@gmail.com>
2025-09-04 23:09:48 +0530

    T021 - Updated Resident Controller with valid comments

--- src/main/java/carveo_portal/carveoManagement/controller/ResidentController.java
+++ src/main/java/carveo_portal/carveoManagement/controller/ResidentController.java
@@ -3,6 +3,7 @@ package carveo_portal.carveoManagement.controller;
 import carveo_portal.carveoManagement.entity.Resident;
 import carveo_portal.carveoManagement.exceptionHandling.ResourceNotFoundException;
 import carveo_portal.carveoManagement.service.ResidentService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,50 +20,51 @@ public class ResidentController {
         this.residentService = residentService;
     }
 
-    @PostMapping("/residents")
-    public Resident saveResident(@RequestBody Resident resident) {
-        // Link each vehicle back to this resident
-        resident.getVehicleList().forEach(vehicle -> vehicle.setResident(resident));
-
-        return residentService.saveResident(resident);
+    // API to save Resident
+    @PostMapping("/saveResident")
+    public ResponseEntity<?> addResident(@Valid @RequestBody Resident resident) {
+        residentService.saveResident(resident);
+        return new ResponseEntity<>("Data Saved", HttpStatus.CREATED);
     }
 
+    //API to save list of resident
     @PostMapping("/listofresident")
     public ResponseEntity<List<Resident>> saveAllResidents(@RequestBody List<Resident> residents) {
         return ResponseEntity.ok(residentService.saveAll(residents));
     }
 
+    // API to get Resident by Id
     @GetMapping("/ResidentById/{id}")
     public Resident getResidentById(@PathVariable int id) {
         return residentService.getResidentById(id).orElseThrow(() -> new ResourceNotFoundException("Resident not found with id: " + id));
     }
 
+    // API to get List of Resident
     @GetMapping("/getAllResident")
     public ResponseEntity<List<Resident>> getAllResidents() {
         return ResponseEntity.ok(residentService.getAllResident());
     }
 
+    // API to get Resident by first name and lastname or both
     @GetMapping("/getByName")
-    public ResponseEntity<?> getResidentByFnameAndLname(
-            @RequestParam String fname,
-            @RequestParam String lname) {
+    public ResponseEntity<?> getByName(@RequestParam(required = false) String fname,
+                                       @RequestParam(required = false) String lname) {
 
-
-        // Validation: no numeric values allowed
-        if (!fname.matches("[a-zA-Z]+") || !lname.matches("[a-zA-Z]+")) {
-            return ResponseEntity.badRequest().body("First Name and Last Name should not contain numbers.");
+        // validation: no numbers allowed
+        if ((fname != null && fname.matches(".*\\d.*")) ||
+                (lname != null && lname.matches(".*\\d.*"))) {
+            return ResponseEntity.badRequest().body("Fname/Lname should not contain numbers.");
         }
 
-        //it will find resident from database
-        Resident resident = residentService.getResidentByFnameAndLname(fname, lname);
+        List<Resident> residents = residentService.findByName(fname, lname);
 
-        //if data is not found in db
-        if (resident == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No resident found with the name " + fname + " " + lname);
+        if (residents.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No resident found with given input(s).");
         }
-        return ResponseEntity.ok(resident);
+        return ResponseEntity.ok(residents);
     }
 
+    //API to get resident by registration number
     @GetMapping("/getByRegNum/{regNum}")
     public Resident getResidentByRegNum(@PathVariable String regNum){
         return residentService.getResidentByRegistrationNumber(regNum);
