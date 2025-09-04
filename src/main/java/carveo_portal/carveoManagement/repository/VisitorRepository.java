78efaf679e803710750dd79dc6f9d365b342f065
jagtapK <jagtapkajal54@gmail.com>
2025-09-04 23:07:09 +0530

    T021 - Created Repository to add visitors in db

--- src/main/java/carveo_portal/carveoManagement/repository/VisitorRepository.java
+++ src/main/java/carveo_portal/carveoManagement/repository/VisitorRepository.java
@@ -3,7 +3,10 @@ package carveo_portal.carveoManagement.repository;
 import carveo_portal.carveoManagement.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VisitorRepository extends JpaRepository<Visitor, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface VisitorRepository extends JpaRepository<Visitor, Integer> {
+    List<Visitor> findByVehicleRegistrationNumber(String registrationNumber);
 
 }
