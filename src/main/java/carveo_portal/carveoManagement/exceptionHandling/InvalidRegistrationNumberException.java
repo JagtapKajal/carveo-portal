4c15cf2b99105960f14e2ad0e691cd3359535e3c
jagtapK <jagtapkajal54@gmail.com>
2025-09-04 23:08:55 +0530

    T021 - Added Exception to handle entites with the validation

--- src/main/java/carveo_portal/carveoManagement/exceptionHandling/InvalidRegistrationNumberException.java
+++ src/main/java/carveo_portal/carveoManagement/exceptionHandling/InvalidRegistrationNumberException.java
@@ -2,10 +2,12 @@ package carveo_portal.carveoManagement.exceptionHandling;
 
 public class InvalidRegistrationNumberException extends RuntimeException {
 
+    // method for custom invalid registration number
     public InvalidRegistrationNumberException(String message){
         super(message);
     }
 
+    // method for custom vehicle not found exception
     public static class VehicleNotFoundException extends  RuntimeException{
 
         public VehicleNotFoundException(String message){
--- src/main/java/carveo_portal/carveoManagement/exceptionHandling/ResourceNotFoundException.java
+++ src/main/java/carveo_portal/carveoManagement/exceptionHandling/ResourceNotFoundException.java
@@ -1,6 +1,7 @@
 package carveo_portal.carveoManagement.exceptionHandling;
 
 public class ResourceNotFoundException extends RuntimeException{
+
     public ResourceNotFoundException(String message){
         super(message);
     }}
