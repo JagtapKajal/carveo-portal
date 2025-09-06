285d15ab293c4ec52bb944ce367660d235cfff47
jagtapK <jagtapkajal54@gmail.com>
2025-09-06 14:22:48 +0530

    T026-Created repository to get endtime of visitor and to know visitor is active or not

--- src/main/java/carveo_portal/carveoManagement/repository/VisitorRepository.java
+++ src/main/java/carveo_portal/carveoManagement/repository/VisitorRepository.java
@@ -1,12 +1,25 @@
 package carveo_portal.carveoManagement.repository;
 
 import carveo_portal.carveoManagement.entity.Visitor;
+import carveo_portal.carveoManagement.enums.VisitorType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface VisitorRepository extends JpaRepository<Visitor, Integer> {
+
+    // repo to find by registration number
     List<Visitor> findByVehicleRegistrationNumber(String registrationNumber);
+
+    // repo to get visitor by endtime
+    Optional<Visitor> findTopByVehicleRegistrationNumberOrderByTimeinDesc(String registrationNumber);
+
+    //to get visitor type to find visitor is active or not
+    List<Visitor> findByVisitorTypeInAndIsactivevisitorTrue(List<VisitorType> types);
+
+    // to get visitor is active or not
+    List<Visitor> findByIsactivevisitorTrue();
+
 
 }
