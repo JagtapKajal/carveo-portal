28ab58daa0ba783bb3163e2c3a68413417f13dea
jagtapK <jagtapkajal54@gmail.com>
2025-09-07 13:50:28 +0530

    T031 - Created Schedular - to store Backup of visitor's history

---
+++ src/main/java/carveo_portal/carveoManagement/utility/VisitorScheduler.java
@@ -0,0 +1,29 @@
+package carveo_portal.carveoManagement.utility;
+
+import carveo_portal.carveoManagement.entity.Visitor;
+import carveo_portal.carveoManagement.repository.VisitorRepository;
+import carveo_portal.carveoManagement.service.VisitorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class VisitorScheduler {
+
+    @Autowired
+    private VisitorRepository visitorRepository;
+
+    @Scheduled(cron = "0 * * * * ?") // every day at 11 PM
+    public void generateDailyVisitorReport() {
+        List<Visitor> visitors = visitorRepository.findAll();
+
+        try {
+            ExcelGenerator.generateVisitorReport(visitors);
+            System.out.println("Visitor report generated successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
