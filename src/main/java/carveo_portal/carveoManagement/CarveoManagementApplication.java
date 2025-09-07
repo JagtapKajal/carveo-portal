0fc4eec5d341650eb566652e5f2996afe65965e3
jagtapK <jagtapkajal54@gmail.com>
2025-09-07 14:49:44 +0530

    T031 -  Added Anotation @EnableScheduling

--- src/main/java/carveo_portal/carveoManagement/CarveoManagementApplication.java
+++ src/main/java/carveo_portal/carveoManagement/CarveoManagementApplication.java
@@ -2,8 +2,10 @@ package carveo_portal.carveoManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CarveoManagementApplication {
 
 	public static void main(String[] args) {
