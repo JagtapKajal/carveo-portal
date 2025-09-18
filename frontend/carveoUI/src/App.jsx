4f976e90186bd0c86175ea4f2b8409f00bf0693b
jagtapK <jagtapkajal54@gmail.com>
2025-09-18 20:17:45 +0530

    added new class visitor

--- frontend/carveoUI/src/App.jsx
+++ frontend/carveoUI/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Resident from './addResident/Resident'
 import './App.css'
+import Visitor from './addVisitor/Visitor'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <div>
         <Resident/>
+        <Visitor/>
        
       </div>
       <p className="read-the-docs">
