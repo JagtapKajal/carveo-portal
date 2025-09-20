91486f74b0892b4ca5f66e80cfa8f8ac3e500325
jagtapK <jagtapkajal54@gmail.com>
2025-09-20 12:45:27 +0530

    Updated vehicle APIs

--- src/main/java/carveo_portal/carveoManagement/controller/VehicleController.java
+++ src/main/java/carveo_portal/carveoManagement/controller/VehicleController.java
@@ -1,11 +1,8 @@
 package carveo_portal.carveoManagement.controller;
 
-import carveo_portal.carveoManagement.entity.Resident;
 import carveo_portal.carveoManagement.entity.Vehicle;
-import carveo_portal.carveoManagement.entity.Visitor;
-import carveo_portal.carveoManagement.service.VehicleService;
+import carveo_portal.carveoManagement.serviceImpl.VehicleServiceImpl;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,22 +10,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
 
-    private final VehicleService vehicleService;
+    private final VehicleServiceImpl vehicleServiceImpl;
 
-    public VehicleController(VehicleService vehicleService) {
-        this.vehicleService = vehicleService;
+    public VehicleController(VehicleServiceImpl vehicleServiceImpl) {
+        this.vehicleServiceImpl = vehicleServiceImpl;
     }
 
     // API to save/add Vehicle entity
     @PostMapping("/createVehicle")
     public ResponseEntity<?> createVehicle(@Valid @RequestBody Vehicle vehicle) {
-         vehicleService.createVehicle(vehicle);
+         vehicleServiceImpl.createVehicle(vehicle);
          return new ResponseEntity<>("Data saved", HttpStatus.CREATED);
     }
 
