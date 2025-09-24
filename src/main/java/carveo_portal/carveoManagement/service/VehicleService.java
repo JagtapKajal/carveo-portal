a14d3e8bb93a00fd87be912fc2d7bedbc948d62c
jagtapK <jagtapkajal54@gmail.com>
2025-09-24 09:53:19 +0530

    Implemented method

--- src/main/java/carveo_portal/carveoManagement/service/VehicleService.java
+++ src/main/java/carveo_portal/carveoManagement/service/VehicleService.java
@@ -1,12 +1,16 @@
 package carveo_portal.carveoManagement.Service;
 
+import carveo_portal.carveoManagement.entity.Resident;
 import carveo_portal.carveoManagement.entity.Vehicle;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface VehicleService {
 
     // to save Vehicle with Validation
-    public ResponseEntity<?> createVehicle(Vehicle vehicle);
+    public Vehicle createVehicle(Vehicle vehicle);
+
 
 
 }
