768e04dc88d9a28aa61d577e24e48d02b1b64d26
jagtapK <jagtapkajal54@gmail.com>
2025-09-29 18:19:01 +0530

    Updated

---
+++ frontend/carveoUI/src/Components/Vehicle/Vehicle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Vehicle = () => {
+  return (
+    <div>
+      <h1>Vehicle Page</h1>
+      <p>Here you can manage vehicle details.</p>
+    </div>
+  );
+};
+
+export default Vehicle;
