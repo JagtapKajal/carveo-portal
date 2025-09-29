888c25967de203bbca26198e66338b7e5da301cf
jagtapK <jagtapkajal54@gmail.com>
2025-09-29 18:05:05 +0530

    Updated

--- frontend/carveoUI/src/App.jsx
+++ frontend/carveoUI/src/App.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Resident from "./Components/addResident/Resident";
-import Visitor from "./Components/addVisitor/Visitor";
-import Vehicle from "./Components/addVehicle/Vehicle";
-import Navbar from "./Components/Navbar";  // 👈 create
-import Dashboard from "./Components/Dashboard"; 
+import Navbar from "./Components/navbar/Navbar";
+import Dashboard from "./Components/dashboard/Dashboard";
+import Resident from "./Components/Resident/Resident";
+import Vehicle from "./Components/Vehicle/Vehicle";
+import Visitor from "./Components/Visitor/Visitor";
+
 function App() {
   return (
     <Router>
-      {/* Navbar stays at top */}
-      <Navbar />
-
-      {/* Page container */}
-      <div className="p-6">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/residents" element={<Resident />} />
-          <Route path="/vehicles" element={<Vehicle />} />
-          <Route path="/visitors" element={<Visitor />} />
-        </Routes>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/resident" element={<Resident />} />
+            <Route path="/vehicle" element={<Vehicle />} />
+            <Route path="/visitor" element={<Visitor />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
