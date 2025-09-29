27e8f02c574e5add1755b2d7e1fa1c3053be0f8b
jagtapK <jagtapkajal54@gmail.com>
2025-09-29 18:18:07 +0530

    Created visitor entity

---
+++ frontend/carveoUI/src/Components/Visitor/Visitor.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Visitor = () => {
+  return (
+    <div>
+      <h1>Visitor Page</h1>
+      <p>Here you can manage visitor details.</p>
+    </div>
+  );
+};
+
+export default Visitor;
