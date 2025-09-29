f02502be3ebfd4b3e64a81bc2bf75086b726cfa1
jagtapK <jagtapkajal54@gmail.com>
2025-09-29 18:09:19 +0530

    Created dashboard t oimplement home page

---
+++ frontend/carveoUI/src/Components/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to Vehicle Management System</h1>
+      <p>Select a section from the navbar to continue.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
