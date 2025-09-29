abd8e00813f04fbf6cbad8560beecdf92dd98150
jagtapK <jagtapkajal54@gmail.com>
2025-09-29 18:08:03 +0530

    Created Navbar with resident, vehicle and visitor and homepage

---
+++ frontend/carveoUI/src/Components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/" className="brand">
+          🚗 Vehicle Management System
+        </Link>
+      </div>
+      <div className="navbar-right">
+        <Link to="/resident" className="nav-link">Resident</Link>
+        <Link to="/vehicle" className="nav-link">Vehicle</Link>
+        <Link to="/visitor" className="nav-link">Visitor</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
