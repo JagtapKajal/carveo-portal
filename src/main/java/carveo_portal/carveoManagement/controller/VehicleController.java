b78c4584ef691200c1429fd07989b75d50062d36
jagtapK <jagtapkajal54@gmail.com>
2025-09-30 23:02:28 +0530

    written APIs to the controller to get all vehicle details

--- src/main/java/carveo_portal/carveoManagement/controller/VehicleController.java
+++ src/main/java/carveo_portal/carveoManagement/controller/VehicleController.java
@@ -1,6 +1,7 @@
 package carveo_portal.carveoManagement.controller;
 
 import carveo_portal.carveoManagement.Service.VehicleService;
+import carveo_portal.carveoManagement.VehicleDTO;
 import carveo_portal.carveoManagement.entity.Vehicle;
 import carveo_portal.carveoManagement.serviceImpl.VehicleServiceImpl;
 import jakarta.validation.Valid;
@@ -9,8 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/vehicles")
 public class VehicleController {
 
@@ -22,6 +25,17 @@ public class VehicleController {
     public ResponseEntity<Vehicle> createVehicle(@Valid @RequestBody Vehicle vehicle) {
          Vehicle savevehicle = vehicleService.createVehicle(vehicle);
          return new ResponseEntity<>(savevehicle, HttpStatus.CREATED);
+    }
+
+//    @GetMapping("/getAllVehicle")
+//    public ResponseEntity<List<Vehicle>> getAllVehicles(){
+//        return ResponseEntity.ok(vehicleService.getAllVehicles());
+//    }
+
+    @GetMapping("/getAllVehicles")
+    public ResponseEntity<List<VehicleDTO>> getAllVehicle() {
+        List<VehicleDTO> vehicleDTOList = vehicleService.getAllVehicle();
+        return new ResponseEntity<>(vehicleDTOList,HttpStatus.OK);
     }
 
 }
