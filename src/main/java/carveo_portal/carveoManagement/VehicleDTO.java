7b020a82d5c24fb7c2b07846dcbe07ff28faef16
jagtapK <jagtapkajal54@gmail.com>
2025-09-30 23:03:48 +0530

    Added new DTOs to get mandatory fields from these DTO files

--- src/main/java/carveo_portal/carveoManagement/VehicleDTO.java
+++ src/main/java/carveo_portal/carveoManagement/VehicleDTO.java
@@ -1,4 +1,109 @@
 package carveo_portal.carveoManagement;
 
+import carveo_portal.carveoManagement.enums.VehicleType;
+
+import java.time.LocalDateTime;
+
 public class VehicleDTO {
+
+        private Long id;
+        private String registrationnumber;
+        private String vname;
+        private String color;
+        private VehicleType type;
+        private LocalDateTime intime;
+        private LocalDateTime outtime;
+        private boolean isvehicleactive;
+
+        private Long residentId;      // For reference
+        private String residentName;  // New field
+        private String flatno;        // Optional, if you want to show flat number
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRegistrationnumber() {
+        return registrationnumber;
+    }
+
+    public void setRegistrationnumber(String registrationnumber) {
+        this.registrationnumber = registrationnumber;
+    }
+
+    public String getVname() {
+        return vname;
+    }
+
+    public void setVname(String vname) {
+        this.vname = vname;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public void setType(VehicleType type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getIntime() {
+        return intime;
+    }
+
+    public void setIntime(LocalDateTime intime) {
+        this.intime = intime;
+    }
+
+    public LocalDateTime getOuttime() {
+        return outtime;
+    }
+
+    public void setOuttime(LocalDateTime outtime) {
+        this.outtime = outtime;
+    }
+
+    public boolean isIsvehicleactive() {
+        return isvehicleactive;
+    }
+
+    public void setIsvehicleactive(boolean isvehicleactive) {
+        this.isvehicleactive = isvehicleactive;
+    }
+
+    public Long getResidentId() {
+        return residentId;
+    }
+
+    public void setResidentId(Long residentId) {
+        this.residentId = residentId;
+    }
+
+    public String getResidentName() {
+        return residentName;
+    }
+
+    public void setResidentName(String residentName) {
+        this.residentName = residentName;
+    }
+
+    public String getFlatno() {
+        return flatno;
+    }
+
+    public void setFlatno(String flatno) {
+        this.flatno = flatno;
+    }
 }
--- src/main/java/carveo_portal/carveoManagement/VisitorDTO.java
+++ src/main/java/carveo_portal/carveoManagement/VisitorDTO.java
@@ -1,4 +1,123 @@
 package carveo_portal.carveoManagement;
 
+import carveo_portal.carveoManagement.enums.VisitorType;
+
+import java.time.LocalDateTime;
+
 public class VisitorDTO {
+
+    private int id;
+    private String visitorname;
+
+    private String vehiclename;
+    private String vehicleRegistrationNumber;
+
+    private String visitpurpose;
+    private LocalDateTime timein;
+
+    private LocalDateTime timeout;
+    private Long phonenumber;
+
+    private VisitorType visitorType;
+   private String flatno;
+
+   private String residentname;
+
+   private Long ResidentId;
+
+    public String getResidentname() {
+        return residentname;
+    }
+
+    public void setResidentname(String residentname) {
+        this.residentname = residentname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVisitorname() {
+        return visitorname;
+    }
+
+    public void setVisitorname(String visitorname) {
+        this.visitorname = visitorname;
+    }
+
+    public String getVehiclename() {
+        return vehiclename;
+    }
+
+    public void setVehiclename(String vehiclename) {
+        this.vehiclename = vehiclename;
+    }
+
+    public String getVehicleRegistrationNumber() {
+        return vehicleRegistrationNumber;
+    }
+
+    public void setVehicleRegistrationNumber(String vehicleRegistrationNumber) {
+        this.vehicleRegistrationNumber = vehicleRegistrationNumber;
+    }
+
+    public String getVisitpurpose() {
+        return visitpurpose;
+    }
+
+    public void setVisitpurpose(String visitpurpose) {
+        this.visitpurpose = visitpurpose;
+    }
+
+    public LocalDateTime getTimein() {
+        return timein;
+    }
+
+    public void setTimein(LocalDateTime timein) {
+        this.timein = timein;
+    }
+
+    public LocalDateTime getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(LocalDateTime timeout) {
+        this.timeout = timeout;
+    }
+
+    public Long getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(Long phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public VisitorType getVisitorType() {
+        return visitorType;
+    }
+
+    public void setVisitorType(VisitorType visitorType) {
+        this.visitorType = visitorType;
+    }
+
+    public String getFlatno() {
+        return flatno;
+    }
+
+    public void setFlatno(String flatno) {
+        this.flatno = flatno;
+    }
+
+    public Long getResidentId() {
+        return ResidentId;
+    }
+
+    public void setResidentId(Long residentId) {
+        ResidentId = residentId;
+    }
 }
