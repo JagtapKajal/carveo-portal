e8f9ee1048583fa141383f8a6f11dc5aeac87fa4
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-19 18:11:33 +0530

    added entities in class

--- src/main/java/carveo_portal/carveoManagement/entity/User.java
+++ src/main/java/carveo_portal/carveoManagement/entity/User.java
@@ -1,4 +1,15 @@
 package carveo_portal.carveoManagement.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
 }
