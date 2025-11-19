abc62aa11b091471e4f937471612c30ea14255b2
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-19 18:34:20 +0530

    added new method in user repo

--- src/main/java/carveo_portal/carveoManagement/repository/UserRepository.java
+++ src/main/java/carveo_portal/carveoManagement/repository/UserRepository.java
@@ -2,6 +2,13 @@ package carveo_portal.carveoManagement.repository;
 
 import carveo_portal.carveoManagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Override
+    Optional<User> findByUsername(String username);
 }
