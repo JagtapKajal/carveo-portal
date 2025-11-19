9f4cc69bfd8d2d3843f7bd5fec30c452c0783ea6
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-19 18:09:16 +0530

    created new class and added security configuration

---
+++ src/main/java/carveo_portal/carveoManagement/security/SecurityConfig.java
@@ -0,0 +1,64 @@
+package carveo_portal.carveoManagement.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.Security;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
+        http.csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
+                    request.anyRequest().authenticated();
+                }).authenticationProvider(authenticationProvider()).httpBasic(Customizer.withDefaults());
+
+        return http.build();
+}
+
+@Bean
+public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+}
+
+  //  @Bean
+   // public UserDetailsService userDetailsService(){
+     //   UserDetails userDetails = User.builder()
+   //             .username("admin")
+       //         .password(passwordEncoder()
+   //                     .encode("admin")).roles("ADMIN").build();
+
+   //     return  new InMemoryUserDetailsManager(userDetails);
+   // }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+}
