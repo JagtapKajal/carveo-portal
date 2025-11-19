313475e7c030a408765629b6b5b6a711b8f89441
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-19 18:34:39 +0530

    Implemented method

--- src/main/java/carveo_portal/carveoManagement/service/UserService.java
+++ src/main/java/carveo_portal/carveoManagement/service/UserService.java
@@ -4,10 +4,15 @@ import carveo_portal.carveoManagement.entity.User;
 import carveo_portal.carveoManagement.repository.UserRepository;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -16,4 +21,11 @@ public class UserService {
        return  userRepository.save(user);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       Optional<User> byUserName = userRepository.findByUsername(username);
+        if(byUserName.isEmpty()) throw new UsernameNotFoundException("user not found");
+        return byUserName;
+
+    }
 }
