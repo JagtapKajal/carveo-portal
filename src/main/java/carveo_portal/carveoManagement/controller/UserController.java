9b91ecd43b16ca72146e8f8c94ec2ac678c1bfb9
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-19 18:26:36 +0530

    created new API to post data

---
+++ src/main/java/carveo_portal/carveoManagement/controller/UserController.java
@@ -0,0 +1,26 @@
+package carveo_portal.carveoManagement.controller;
+
+import carveo_portal.carveoManagement.entity.User;
+import carveo_portal.carveoManagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        return userService.createUser(user);
+
+    }
+}
