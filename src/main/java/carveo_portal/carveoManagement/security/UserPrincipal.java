7d75c9088dd2a3fb74b1cabdc0bc31b22924b27e
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-20 15:00:08 +0530

    created user principal class to implement username and password methods

--- src/main/java/carveo_portal/carveoManagement/security/UserPrincipal.java
+++ src/main/java/carveo_portal/carveoManagement/security/UserPrincipal.java
@@ -1,6 +1,38 @@
 package carveo_portal.carveoManagement.security;
 
-public class UserPrincipal {
+import carveo_portal.carveoManagement.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    //this class will print user details like - username, password
+
+    private User user;
+
+    public UserPrincipal(User user){
+        this.user = user;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+
 
 
 }
