c9781e057233954f3c56b35253a04e729d21d936
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-20 14:58:39 +0530

    implemented all necessary security credentials

--- src/main/java/carveo_portal/carveoManagement/security/SecurityConfig.java
+++ src/main/java/carveo_portal/carveoManagement/security/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
         http.csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(request -> {
+                    request.requestMatchers( "/user/register").permitAll();
                     request.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
                     request.anyRequest().authenticated();
                 }).authenticationProvider(authenticationProvider()).httpBasic(Customizer.withDefaults());
