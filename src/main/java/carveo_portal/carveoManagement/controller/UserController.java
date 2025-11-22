cb62b01f1a396f71a4e830d050d0ca7633af5b9c
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-22 12:12:17 +0530

    Implemented API to get jwt token

--- src/main/java/carveo_portal/carveoManagement/controller/UserController.java
+++ src/main/java/carveo_portal/carveoManagement/controller/UserController.java
@@ -1,27 +1,49 @@
 package carveo_portal.carveoManagement.controller;
 
+import carveo_portal.carveoManagement.dto.UserDTO;
 import carveo_portal.carveoManagement.entity.User;
 import carveo_portal.carveoManagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Autowired
     private UserService userService;
 
 
     @PostMapping("/register")
-    public User register(@RequestBody User user){
+    public User register(@RequestBody User user) {
         return userService.createUser(user);
+    }
+    @PostMapping("/login")
+    public String login(@RequestBody UserDTO user){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(),
+                        user.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
+        List<String> roles = authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+
+       UserDetails userDetails =  userService.loadUserByUsername(user.getUsername());
 
     }
 }
