418c56022e94ac36917cc6d74aea13e6985049e2
jagtapkajal <jagtapkajal54@gmail.com>
2025-11-22 16:00:02 +0530

    added BCryptencode to check jwt token is working or not

--- src/main/java/carveo_portal/carveoManagement/CarveoManagementApplication.java
+++ src/main/java/carveo_portal/carveoManagement/CarveoManagementApplication.java
@@ -3,6 +3,7 @@ package carveo_portal.carveoManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +11,7 @@ public class CarveoManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarveoManagementApplication.class, args);
+        System.out.println(new BCryptPasswordEncoder(12).encode("admin"));
 	}
 
 }
